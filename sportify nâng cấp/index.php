c695fd7772bd62c9fe277054f6e8e697d9a8690e
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-10 17:08:30 +0700

    Update index.php

--- sportify nâng cấp/index.php
+++ sportify nâng cấp/index.php
@@ -2,7 +2,6 @@
 <?php
 session_start();
 //tiến hành kiểm tra là người dùng đã đăng nhập hay chưa
-//nếu chưa, chuyển hướng người dùng ra lại trang đăng nhập
 if (!isset($_SESSION['username'])) {
 	 header('Location: login.php');
 }
