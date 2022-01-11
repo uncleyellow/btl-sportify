bc6947a319ef5ba02aa0c6534384c632b2d7dffe
31-1951060751-DaoDucHuy <94445988+31-1951060751-DaoDucHuy@users.noreply.github.com>
2022-01-11 22:32:30 +0700

    Update footer.php

--- sportify nâng cấp/Sources/footer.php
+++ sportify nâng cấp/Sources/footer.php
@@ -1,1 +1,39 @@
-
+<footer>
+<div class="dshow"><img class="float-left mr-3" src="./image/167.jpg" width="100px">
+    <h4>Mãi Mãi Bên Nhau</h4>
+    <label for="">Trình bày:</label>
+    Noo Phước Thịnh
+    <br>
+    <label for="">Lượt nghe:</label>
+    <div id="player" class="position-relative h-100"> 
+      <div class="ap" id="ap">  
+        <div class="ap-inner">    
+        <div class="ap-panel">      
+          <div class="ap-item ap--playback">        
+          <button class="ap-controls ap-prev-btn">          
+            <i class="material-icons md-dark">skip_previous</i>        
+          </button>        
+          <button class="ap-controls ap-toggle-btn">            
+            <i class="material-icons md-dark ap--pause">play_arrow</i>        
+          </button>        
+          <button class="ap-controls ap-next-btn">          
+            <i class="material-icons md-dark">skip_next</i>        
+          </button>      
+        </div>      
+        <div class="ap-item ap--track">        
+          <div class="ap-info">          
+            <div class="ap-title">Hongkong1</div>          
+            <div class="ap-time">            
+              <span class="ap-time--current">--</span>            
+              <span> / </span>            
+              <span class="ap-time--duration">--</span>          
+            </div>          
+            <div class="ap-progress-container">            
+              <div class="ap-progress">              
+                <div class="ap-bar"></div>              
+                <div class="ap-preload-bar"></div>            
+              </div>          
+            </div>        
+          </div>      
+        </div>      
+          </footer>
