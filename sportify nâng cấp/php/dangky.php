66d8b0ca121e91d8c4a4d9c68cc138d945ca70a0
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 22:47:57 +0700

    Update dangky.php

--- sportify nâng cấp/php/dangky.php
+++ sportify nâng cấp/php/dangky.php
@@ -4,3 +4,8 @@
     $password   = addslashes($_POST['txtPassword']);
     $email      = addslashes($_POST['txtEmail']);
     $passwordcf = addslashes($_POST['txtPasswordCF']);
+ if ($password!=$passwordcf){
+      echo 'Mật khẩu và mật khẩu xác nhận không khớp. Vui lòng nhập lại. <a href="javascript: history.go(-1)">Trở lại</a> sau... <span id="time"></span>';
+      echo '<script src="../js/demnguoc.js" charset="utf-8"></script>';
+      exit;
+    }
