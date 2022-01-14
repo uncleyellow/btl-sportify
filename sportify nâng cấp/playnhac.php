78d75001f04097b907044ce604b62e0cd417add9
31-1951060751-DaoDucHuy <94445988+31-1951060751-DaoDucHuy@users.noreply.github.com>
2022-01-15 02:44:22 +0700

    Update playnhac.php

--- sportify nâng cấp/playnhac.php
+++ sportify nâng cấp/playnhac.php
@@ -71,4 +71,12 @@
 		$sql1=mysqli_query($con,"select*from v_baihat where id='$id'");
 		$rown1=mysqli_fetch_assoc($sql1);
 		$sql=mysqli_query($con,"SELECT * FROM v_baihat WHERE idalbum = '$rown1[idalbum]' AND id != '$id' ORDER BY ngaydang DESC LIMIT 20");
+	echo "<script>
+        var iconImage = null;
+        AP.init({
+            container:'#player',
+            volume   : 0.7,
+            autoPlay : false,
+            notification: false,
+			playList: [";
 
