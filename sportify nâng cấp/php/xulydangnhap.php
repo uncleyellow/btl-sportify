821a57b2dbbd14c1ee1e2c9c65bf5b4fc8349c7f
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 22:52:55 +0700

    xulydangnhap.php

---
+++ sportify nâng cấp/php/xulydangnhap.php
@@ -0,0 +1,6 @@
+<?php
+    session_start();
+    include('./connect.php');
+    $userName = $_POST['txtlogin'] ;
+    $passWord = $_POST['txtpasswordlogin'];
+    $result = mysqli_query($con,"Select * from user where userName = '$userName'");
