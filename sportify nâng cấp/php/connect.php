1faf2732cb557e56c37e9d2abc5f4637239fab6b
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 22:58:43 +0700

    Kết nối CSDL spotify

--- sportify nâng cấp/php/connect.php
+++ sportify nâng cấp/php/connect.php
@@ -7,3 +7,8 @@ $dbname = "spotify";
 $con = mysqli_connect($servername,$username,$password,$dbname);
 
 mysqli_set_charset($con,"utf8");
+
+if(!$con){
+   die('Kết nối thất bại:'.mysqli_connect_error());
+}
+?>
