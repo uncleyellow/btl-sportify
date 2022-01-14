0b4f1316393f28d482d5d41d494e3223af63305f
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 23:16:53 +0700

    Update baihat.php

--- sportify nâng cấp/admin/baihat.php
+++ sportify nâng cấp/admin/baihat.php
@@ -16,3 +16,17 @@
     <main class="col-md-10 m-auto">
     <div class="mt-3 mb-2 text-center"><a href="./thembaihat.php" class="btn btn-primary">Thêm bài hát</a></div>
     <div class="card mb-3 mt-3">
+ <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th >STT</th>
+                            <th>Tên Bài Hát</th>
+                            <th>Lượt Nghe</th>
+                            <th>Ca sĩ</th>
+                            <th>Album</th>
+                            <th>Chủ đề</th>
+                            <th>Ngày đăng</th>
+                            <th>Xoá</th>
+                        </tr>
