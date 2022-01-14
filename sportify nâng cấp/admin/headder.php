453cae3ee3e16e305b27eb7e805c33adfaa6922d
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 23:21:55 +0700

    headder trong quan tri

---
+++ sportify nâng cấp/admin/headder.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="./css/header.css">
+<header style="position: fixed;top:0;width:100%;z-index:1;">
+    <nav class="navbar navbar-default navbar-expand-lg navbar-light">
+        <div class="navbar-header d-flex col">
+            <a class="navbar-brand" href="../index.php"><span><img class="mr-2" src="../image/logo.png" width="150px"></span></a>
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle navbar-toggler ml-auto">
+                <span class="navbar-toggler-icon"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
