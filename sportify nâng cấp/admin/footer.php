31ca95bd21e5c376ea135306108e10b8b7011800
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 23:24:20 +0700

    footer trong web quản trị

---
+++ sportify nâng cấp/admin/footer.php
@@ -0,0 +1,8 @@
+<footer style="position: fixed;bottom:0;width:100%;">
+<div class="container-fluid border-top mt-3 bg-white">
+      <div class="row">
+        <div class="col-md-12 text-center d-md-flex align-items-center"> <i class="d-block fa-2x mr-md-5 text-primary"></i>
+          <ul class="nav mx-md-auto d-flex justify-content-center">
+            <li class="nav-item"> <a class="nav-link active" href="../index.php">Home</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
+          </ul>
