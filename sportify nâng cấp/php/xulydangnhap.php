22c3907ecfc0d9bcfa99a617725c9cba435af8e5
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 22:53:50 +0700

    Update xulydangnhap.php

--- sportify nâng cấp/php/xulydangnhap.php
+++ sportify nâng cấp/php/xulydangnhap.php
@@ -8,3 +8,11 @@ if (mysqli_num_rows($result)==1) {
       $row = mysqli_fetch_assoc($result);
       $hash = $row["passWord"];
       $_SESSION['level'] = $row['level'];
+     if(password_verify($passWord, $hash))
+        {
+            $_SESSION['userName']=$userName;
+            $_SESSION['id']=$row['id'];
+            $_SESSION['avatar']=$row['avatar'];
+            header("location:../index.php");
+            exit();
+        }
