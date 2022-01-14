29c81ea03eee283aad88b4a3875d3896456292a9
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 22:53:29 +0700

    Update xulydangnhap.php

--- sportify nâng cấp/php/xulydangnhap.php
+++ sportify nâng cấp/php/xulydangnhap.php
@@ -4,3 +4,7 @@
     $userName = $_POST['txtlogin'] ;
     $passWord = $_POST['txtpasswordlogin'];
     $result = mysqli_query($con,"Select * from user where userName = '$userName'");
+if (mysqli_num_rows($result)==1) {
+      $row = mysqli_fetch_assoc($result);
+      $hash = $row["passWord"];
+      $_SESSION['level'] = $row['level'];
