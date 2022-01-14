f1cae6b8ffb14d105103753646ccb27d5562a2ad
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 23:11:58 +0700

    Update album.php

--- sportify nâng cấp/admin/album.php
+++ sportify nâng cấp/admin/album.php
@@ -17,3 +17,14 @@
     <main class="col-md-10 m-auto">
     <div class="mt-3 mb-2 text-center"><a href="./themalbum.php" class="btn btn-primary">Thêm album</a></div>
     <div class="card mb-3 mt-3">
+<div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>Tên album</th>
+                            <th>Sửa</th>
+                            <th>Xoá</th>
+                        </tr>
+                    </thead>
