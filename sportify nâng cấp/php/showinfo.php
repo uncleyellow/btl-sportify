0aa7bd7d0474915134a89cccc28343801cda2ad6
31-1951060751-DaoDucHuy <94445988+31-1951060751-DaoDucHuy@users.noreply.github.com>
2022-01-14 23:17:59 +0700

    Update showinfo.php

--- sportify nâng cấp/php/showinfo.php
+++ sportify nâng cấp/php/showinfo.php
@@ -1,1 +1,12 @@
-
+<?php
+    $id=$_POST['id'];
+    include('./connect.php');
+    $sql2=mysqli_query($con,"select*from v_baihat where id='$id'");
+    $row2=mysqli_fetch_assoc($sql2);
+    mysqli_close($con);
+    echo'<img class ="float-left mr-3" src="./'.$row2['image'].'" width="100px">
+    <h4>'.$row2['tenbaihat'].'</h4>
+    '.$row2['tencasi'].'
+    <br>';
+    echo"</div>";
+?>
