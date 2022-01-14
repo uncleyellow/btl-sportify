086492990ad528c64d96858afdd27cd6f5b99fad
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 23:07:15 +0700

    Update admin.php

--- sportify nâng cấp/admin/admin.php
+++ sportify nâng cấp/admin/admin.php
@@ -8,3 +8,21 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/dataTables.bootstrap4.css" rel="stylesheet">
 </head>
+    
+    <body>
+<?php
+    session_start();
+
+    if($_SESSION['level']==2)
+    {
+        include('header.php');?>
+        <main class="col-md-10 m-auto">
+    <div class="card mb-3 mt-3">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
