d7e852b356feee76655ecf42cfa49917212c3603
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-14 23:06:45 +0700

    Add files via upload

---
+++ sportify nâng cấp/admin/admin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trang quản trị</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/dataTables.bootstrap4.css" rel="stylesheet">
+</head>
