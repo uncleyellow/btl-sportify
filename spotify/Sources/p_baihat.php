2ed93032c6c2efb132d9b015a8057969bfdc4915
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-15 17:46:08 +0700

    Update p_baihat.php

--- spotify/Sources/p_baihat.php
+++ spotify/Sources/p_baihat.php
@@ -6,14 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bài hát</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="./css/hover.css">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
-	<link rel="stylesheet" href="./css/jquery.paginate.css">
     <script src="./js/jquery.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
-	<script src="./js/jquery.paginate.js"></script>
-    <script src="./js/like.js"></script>
 </head>
 
 <body>
