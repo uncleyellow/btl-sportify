d8b5b45894e3049964101449c8f9d4117ded5048
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-15 17:29:31 +0700

    Delete sportify nâng cấp directory

--- sportify nâng cấp/admin/del_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $id = $_GET["id"];
-    include('../php/connect.php');
-    mysqli_query($con,"Delete from user where id = '$id'");
-    echo "Xoá thành công";
-    header('location:user.php');
-
-    mysqli_close($con);
-?>
