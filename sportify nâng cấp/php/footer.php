334ed2bd42af7a2611bd7792f5b6153b2ae46ec5
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-15 11:50:20 +0700

    footer ca php

---
+++ sportify nâng cấp/php/footer.php
@@ -0,0 +1,4 @@
+<footer style="position: fixed; bottom:0; width:100%;">
+<div class="container-fluid border-top mt-3 bg-white">
+      <div class="row">
+        <div class="col-md-12 text-center d-md-flex align-items-center"> <i class="d-block fa-2x mr-md-5 text-primary"></i>
