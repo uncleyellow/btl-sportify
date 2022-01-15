121e560ad80aa582930b68fa949e601325ebc721
uncleyellow <92460013+uncleyellow@users.noreply.github.com>
2022-01-15 11:33:18 +0700

    Update album.php

--- sportify nâng cấp/album.php
+++ sportify nâng cấp/album.php
@@ -26,3 +26,12 @@
                     $sql = "SELECT * FROM album";
                     $result = mysqli_query($con,$sql);
                     while($row = mysqli_fetch_assoc($result)){
+                        $tenalbum = $row['tenalbum'];
+                        $image=$row['image'];
+                        echo '<div class="col-lg-3 col-md-4 img-hover">
+                                <a href="p_album.php?id='.$row['id'] .'" class="d-block mb-4 h-100" style="text-decoration: none;">
+                                <div><img class="img-fluid img-thumbnail " src="./'.$image.'" alt=""></div>
+                                <div class="mt-2" style="color: black;">'.$tenalbum.'</div>
+                                </a>
+                            </div>';
+                    }
